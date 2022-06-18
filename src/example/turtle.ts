84533be4d2cc4f4b0163dd84bad9bd7059e17459
0990900 <gunam.dev@gmail.com>
2022-06-18 18:22:57 +0900

    modify turtle program

--- src/example/turtle.ts
+++ src/example/turtle.ts
@@ -1,32 +1,32 @@
 type Move = (path: string[]) => void;
 type Turn = (path: string[]) => void;
 type Stop = (path: string[]) => void;
-type TurtleProgram = Move | Turn | Stop;
+type Command = Move | Turn | Stop;
 
 const move =
-  (meter: number, next?: TurtleProgram): Move =>
+  (meter: number, next?: Command): Move =>
   (path: string[]) => {
     path.push(`Move ${meter} meters`);
     next && next(path);
   };
 
 const turn =
-  (degree: number, next?: TurtleProgram): Turn =>
+  (degree: number, next?: Command): Turn =>
   (path: string[]) => {
     path.push(`Turn ${degree} degrees`);
     next && next(path);
   };
 
 const stop =
-  (next?: TurtleProgram): Stop =>
+  (next?: Command): Stop =>
   (path: string[]) => {
     path.push('Stop');
     next && next(path);
   };
 
-const compiler = (c: TurtleProgram): Array<string> => {
+const compiler = (program: Command): Array<string> => {
   const path = [] as Array<string>;
-  c(path);
+  program(path);
   return path;
 };
 
